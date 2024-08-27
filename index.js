b26401c5090fb6cc51042f2243cb5e3c218e5c85
Asim Shakoor <asimshakoor920830@gmail.com>
2024-08-27 23:08:58 +0500

    index.js

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ const port = 5000
 const mongodb = require('./db')
 const cors = require('cors')
 
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://food-frontend-sand.vercel.app");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
-})
+// app.use((req,res,next)=>{
+//   res.setHeader("Access-Control-Allow-Origin","https://food-frontend-sand.vercel.app");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   next();
+// })
 
-app.use(cors(
-  {
-    origin: ["https://food-frontend-sand.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-))
+// app.use(cors(
+//   {
+//     origin: ["https://food-frontend-sand.vercel.app"],
+//     methods: ["POST", "GET"],
+//     credentials: true
+//   }
+// ))
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(cors());
 
 mongodb();
 
